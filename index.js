3c3756886ad6c4160d7ec9803537155741e23cd5
GUILHERME VICTOR BOTA SALOMAO <guilherme.vbsalomao@senacsp.edu.br>
2021-05-22 12:08:50 -0300

    servidor att

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const app = express();
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 app.use(bodyParser.json());
 //  configuração do cors para aceitar varios protocolos de requisiçao
 const configCors = {
@@ -18,78 +20,77 @@ const cx = mysql.createConnection({
   database: "onlatter",
   port: "3306",
 });
-cx.connect((error,dados)=>{
-  if (error){
+cx.connect((error, dados) => {
+  if (error) {
     console.error(`Erro ao tentar executar o servidor -> ${erro.stack}`);
     return;
   }
   console.log(`Dados do servidor -> ${cx.threadId}`);
 });
 // Rotas para o usuario
-app.post("/usuario/cadastro", cors(configCors),(req,res)=>{
+app.post("/usuario/cadastro", cors(configCors), (req, res) => {
   const sh = req.body.senha;
+  const em = req.body.email;
   const us = req.body.nomeusuario;
-  bcrypt.hash(sh, saltRound).then((senha)=>{
+  bcrypt.hash(sh, saltRound).then((senha) => {
     cx.query(
-      "insert to tbusuario set nomeusuario=?,senha=?",
-      [us,senha],
-      (error,result)=>{
-        if (error){
-          res.status(400).send({output:`não cadastrou -> ${error}`});
+      "insert into tbusuario set nomeusuario=?,email=?,senha=?",
+      [us, em, senha],
+      (error, result) => {
+        if (error) {
+          res.status(400).send({ output: `não cadastrou -> ${error}` });
           return;
         }
-        res.status(201).send({output:result});
+        res.status(201).send({ output: result });
       }
     );
   });
 });
-app.get("/usuario/listar",cors(configCors),(req,res)=>{
-  cx.query("selec * from tbusuario", (error,result)=>{
-    if(error){
+app.get("/usuario/listar", cors(configCors), (req, res) => {
+  cx.query("selec * from tbusuario", (error, result) => {
+    if (error) {
       res
-      .status(400)
-      .send({output:`Não foi possivel listar os usuarios ${error}`});
+        .status(400)
+        .send({ output: `Não foi possivel listar os usuarios ${error}` });
       return;
     }
-    res.status(200).send({output:result});
+    res.status(200).send({ output: result });
   });
 });
-app.put("/usuario/alterarsenha/:id", cors(configCors),(req,res)=>{
-  cx.query
+app.put("/usuario/alterarsenha/:id", cors(configCors), (req, res) => {
+  cx.query;
   "update tbusuario set ? whare idusuario=?",
-  [req.body.params.id],
-  (error,result)=>{
-    if (error){
-      res
-      .status(400)
-      .sand({output:`Não foi possivel alterar a senha -> ${error}`});
-      return;
-    }
-    res.status(200).send({output:result});
-  };
+    [req.body.params.id],
+    (error, result) => {
+      if (error) {
+        res
+          .status(400)
+          .sand({ output: `Não foi possivel alterar a senha -> ${error}` });
+        return;
+      }
+      res.status(200).send({ output: result });
+    };
 });
-app.post("/usuario/login",cors(configCors),(req,res)=>{
+app.post("/usuario/login", cors(configCors), (req, res) => {
   const us = req.body.nomeusuario;
   const em = req.body.email;
   const sh = req.body.senha;
 
   cx.query(
     `select u.* from tbusuario u
-  inner join tbcliente cl
-  on u.idusuario = cl.idusuario
-  inner join tbcontato co
-  on cl.idcontato = co.idcontato
-  where (u.nomeusuario=? or cl.cpf=? or co.email=?)`,
-  [us,em],
-  (e,rs)=>{
-    if (e) return res.status(400).send({output: e })
-    bcrypt.compare(sh, rs[0].senha).then((resultado)=>{
-      if (resultado){
-        res.status(200).send({output:"Logado"});
-      } else {
-        res.status(404).send({output: "Usuario não localizado"});
-      }
-    });
-  }
-  )
+  where (u.nomeusuario=? or u.email=?)`,
+    [us, em],
+    (e, rs) => {
+      if (e) return res.status(400).send({ output: e });
+      bcrypt.compare(sh, rs[0].senha).then((resultado) => {
+        if (resultado) {
+          res.status(200).send({ output: "Logado" });
+        } else {
+          res.status(404).send({ output: "Usuario não localizado" });
+        }
+      });
+    }
+  );
 });
+
+app.listen(5005);
